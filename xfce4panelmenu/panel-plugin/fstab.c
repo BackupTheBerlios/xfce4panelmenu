7519eaca1da8b7821068393bc967948ba5cfb62f
rpociecha <rpociecha>
2005-03-06 19:35:02 +0000

    invisible scrollbar fixed

--- xfce4panelmenu/panel-plugin/fstab.c
+++ xfce4panelmenu/panel-plugin/fstab.c
@@ -159,7 +159,7 @@ static void fs_tab_init (FsTabWidget *ft)
 
 	scroll = gtk_scrolled_window_new (NULL, NULL);
 	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scroll),
-					GTK_POLICY_NEVER,
+					GTK_POLICY_AUTOMATIC,
 					GTK_POLICY_AUTOMATIC);
 	gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (scroll),
 					     GTK_SHADOW_NONE);
